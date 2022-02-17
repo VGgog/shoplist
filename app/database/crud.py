2178c4d202d15d7ad048640a6ff7637f3f2ac2a5
Aidar <gluhovkich@gmail.com>
2022-02-18 00:25:03 +0500

    Create delete_document() function
    This function delete document in collection

--- app/database/crud.py
+++ app/database/crud.py
@@ -14,6 +14,14 @@ def find_group(collection, data):
 
 def update_document(collection, element, new_value):
     """
+    Function for update data in collection
     """
     return collection.update_one(element, {'$set': new_value})
+
+
+def delete_document(collection, data):
+    """
+    Function for delete document in collection.
+    """
+    return collection.delete_one(data)
 
