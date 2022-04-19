0f39e7b40f74615b9d3e2bada2539b1071658e31
Aidar <gluhovkich@gmail.com>
2022-04-19 19:44:27 +0500

    Make bot.py a main file

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,19 @@
-from aiogram import Bot, Dispatcher
+import logging
+
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+from handlers.handler import register_handlers
+from keyboards.callbacks import register_buttons_callback
 from config import Config
 
 
 bot = Bot(token=Config.token)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+logging.basicConfig(level=logging.INFO)
+
+register_handlers(dp)
+register_buttons_callback(dp)
+
+executor.start_polling(dp)
